fb9b47791ac92bf5671238ea5e061f47e2bb486d
Saac Sornchai <saacsos@gmail.com>
2019-03-30 15:35:58 +0700

    evoting spring second commit

---
+++ src/main/java/ku/evoting/Party.java
@@ -0,0 +1,60 @@
+package ku.evoting;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Party {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+    private int voteCount;
+    private int electedMemberCount;
+    private int partylistCount;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVoteCount() {
+        return voteCount;
+    }
+
+    public void setVoteCount(int voteCount) {
+        this.voteCount = voteCount;
+    }
+
+    public int getElectedMemberCount() {
+        return electedMemberCount;
+    }
+
+    public void setElectedMemberCount(int electedMemberCount) {
+        this.electedMemberCount = electedMemberCount;
+    }
+
+    public int getPartylistCount() {
+        return partylistCount;
+    }
+
+    public void setPartylistCount(int partylistCount) {
+        this.partylistCount = partylistCount;
+    }
+
+
+}
---
+++ src/main/java/ku/evoting/PartyRepository.java
@@ -0,0 +1,9 @@
+package ku.evoting;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PartyRepository extends JpaRepository<Party, Integer> {
+
+}
